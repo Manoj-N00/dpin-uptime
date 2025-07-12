85abe61b5e398451a1b2d7acfb9173bfb0ce4eff
Manoj-N00 <manojnaikj@gmail.com>
2025-07-12 16:02:49 +0530

    Fix Nav

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from 'next';
 import { createCivicAuthPlugin } from '@civic/auth-web3/nextjs';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: '.next',
   webpack: (config, { isServer }) => {
-    // Prevent Webpack from trying to bundle `pino-pretty`
     config.resolve.fallback = {
       ...config.resolve.fallback,
       'pino-pretty': false,
@@ -15,10 +14,8 @@ const nextConfig: NextConfig = {
 };
 
 const withCivicAuth = createCivicAuthPlugin({
-  // clientId: `771c8ce2-0f4a-4aa3-a14a-d91529afdddf`,
   clientId: `${process.env.NEXT_PUBLIC_CIVIC_CLIENT_ID}`,
   include: ['/dashboard/*'],
-  // oauthServer is not necessary for production.
   oauthServer: process.env.AUTH_SERVER || 'https://auth.civic.com/oauth',
 });
 
